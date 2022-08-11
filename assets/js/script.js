92a9fef3d139ac5924c1b2a91ee08271fd78900d
Kamary <kamarygillespie@Kamarys-MBP.lan>
2022-08-11 15:24:14 -0600

    Filled in quiz info to create 10 question quiz on USA capitals

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,79 @@
-var questionArry = [{
-        question: "",
-        options: [],
-        correctAnswer: "",
+var questionArray = [{
+        question: "What is the capital of California?",
+        options: ["Hartford", "Pierre", "Sacramento", "Austin"],
+        correctAnswer: "Sacramento",
     },
     {
-        question: "",
-        options: [],
-        correctAnswer: "",
+        question: "What is the capital of Colorado?",
+        options: ["Hartford", "Richmond", "Sacramento", "Denver"],
+        correctAnswer: "Denver",
     },
     {
-        question: "",
-        options: [],
-        correctAnswer: "",
+        question: "What is the capital of Texas?",
+        options: ["Boise", "Pierre", "Honolulu", "Austin"],
+        correctAnswer: "Austin",
     },
     {
-        question: "",
-        options: [],
-        correctAnswer: "",
+        question: "What is the capital of Montana?",
+        options: ["Jefferson City", "Helena", "Sacramento", "Cheyenne"],
+        correctAnswer: "Helena",
     },
     {
-        question: "",
-        options: [],
-        correctAnswer: "",
+        question: "What is the capital of Missouri?",
+        options: ["Jefferson City", "Pierre", "Sacramento", "Austin"],
+        correctAnswer: "Jefferson City",
+    },
+    {
+        question: "What is the capital of Missouri?",
+        options: ["Jefferson City", "Pierre", "Sacramento", "Austin"],
+        correctAnswer: "Jefferson City",
+    },
+    {
+        question: "What is the capital of Missouri?",
+        options: ["Jefferson City", "Pierre", "Sacramento", "Austin"],
+        correctAnswer: "Jefferson City",
+    },
+    {
+        question: "What is the capital of Missouri?",
+        options: ["Jefferson City", "Pierre", "Sacramento", "Austin"],
+        correctAnswer: "Jefferson City",
+    },
+    {
+        question: "What is the capital of Missouri?",
+        options: ["Jefferson City", "Pierre", "Sacramento", "Austin"],
+        correctAnswer: "Jefferson City",
     }
 ]
+
+Hartford
+Pierre
+Austin
+Helena
+Providence
+Baton Rouge
+Honolulu
+Raleigh
+Bismarck
+Indianapolis
+Richmond
+Boise
+Jackson
+Sacramento
+Boston
+Jefferson City
+Saint Paul
+Carson City
+Juneau
+Salem
+Charleston
+Lansing
+Salt Lake City
+Cheyenne
+Lincoln
+Santa Fe
+Columbia
+Little Rock
+Springfield
+Columbus
+Madison
+Tallahassee
